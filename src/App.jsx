14188406efbb552d5857f76c0026aadf0d4b80bf
imran2024bd <imranitbd34@gmail.com>
2024-07-06 13:00:15 +0600

    42-1 Knowledge Cafe Project Overview And Tailwind Setup Starting

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-6xl , bg-orange-400'>React Tailwind Setup Knowledge Cafe</h1>
+      
+    </>
+  )
+}
+
+export default App
