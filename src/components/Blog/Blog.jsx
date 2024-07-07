c79e417228e7b401b85f102e40b0aebe9948397d
imran2024bd <imranitbd34@gmail.com>
2024-07-07 15:52:53 +0600

    Bookmark add

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
-
+import { FaBookmark } from 'react-icons/fa';
 const Blog = ({ blog }) => {
     // console.log(blog);
     const { title, author, cover, author_img, reading_time, posted_date, hashtags
     } = blog;
     return (
-        <div>
-            <img src={cover} alt={`cover Picture of the title ${title}`} />
+        <div className='mb-20'>
+            <img className='w-full mb-8 rounded-3xl' src={cover} alt={`cover Picture of the title ${title}`} />
             <div className='flex justify-between'>
                 <div className='flex '>
                     <img className='w-14' src={author_img} alt="" />
@@ -18,11 +18,12 @@ const Blog = ({ blog }) => {
                 </div>
                 <div className='flex items-center '>
                     <span>{reading_time} min read</span>
-                    <button>Bookmarks</button>
+                    <button className='ml-2 text-2xl text-red-600'
+                    > <FaBookmark></FaBookmark></button>
                 </div>
             </div>
-            <h2 className="text-4xl font-bold"> {title} </h2>
-            <p>
+            <h2 className="text-4xl font-bold mt-4"> {title} </h2>
+            <p className='mt-4 gap-x-4'>
                 {
                     hashtags.map((hash, idx) => <span key={idx}> <a href="">#{hash}</a></span>)
                 }
