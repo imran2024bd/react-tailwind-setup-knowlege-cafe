1f641614e1a9a37cb8b0a8c7b7ba47ad80a43446
imran2024bd <imranitbd34@gmail.com>
2024-07-07 11:52:16 +0600

    42-3 Set Layout And Make React Responsive ended

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -2,16 +2,16 @@ import { useEffect, useState } from "react";
 
 
 const Blogs = () => {
-    const [blogs , setBlogs] = useState([])
+    const [blogs, setBlogs] = useState([])
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch('blogs.json')
-        .then(res => res.json())
-        .then(data => setBlogs(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, [])
     return (
-        <div>
-            
+        <div className="md:w-2/3">
+            <h2 className="text-4xl">Blogs: {blogs.length} </h2>
         </div>
     );
 };
